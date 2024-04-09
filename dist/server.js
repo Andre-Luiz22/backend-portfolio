37a74f574513984e05d064515b8f42ce4c00d615
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-04-09 01:25:58 -0300

    removendo a dist e o vercel.json

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./src/app");
-require("dotenv/config");
-const PORT = process.env.PORT;
-app_1.app.listen(PORT, () => {
-    console.log(`server listening on port ${PORT}`);
-});
