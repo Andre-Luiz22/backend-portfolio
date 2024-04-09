25135548df9ba69b3f58adc9f114846e61c47e84
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-04-09 02:04:02 -0300

    mudando o cors

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,10 @@
 import { app } from "./src/app";
 import "dotenv/config";
+import cors from "cors";
 
 const PORT = process.env.PORT;
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
