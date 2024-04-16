8db9184932db1faf1f6dc40bc6ffa4f41e933e11
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2024-04-15 22:59:30 -0300

    comentando codigo

--- app/src/controllers/cardProjetoController.ts
+++ app/src/controllers/cardProjetoController.ts
@@ -13,31 +13,31 @@ export class CardProjetoController {
     }
   };
 
-  static listFavoriteCards = async (req: Request, res: Response) => {
-    try {
-      const favoriteCards = await cardProjeto.find({ favorite: true });
-      res.status(200).json(favoriteCards);
-    } catch (err) {
-      res.status(500).json({ message: "Erro ao buscar as imagens favoritas" });
-    }
-  };
+  // static listFavoriteCards = async (req: Request, res: Response) => {
+  //   try {
+  //     const favoriteCards = await cardProjeto.find({ favorite: true });
+  //     res.status(200).json(favoriteCards);
+  //   } catch (err) {
+  //     res.status(500).json({ message: "Erro ao buscar as imagens favoritas" });
+  //   }
+  // };
 
-  static createNewCard = async (req: Request, res: Response) => {
-    try {
-      const newCarrousselCard = await cardProjeto.create(req.body);
-      res.status(200).json(newCarrousselCard);
-    } catch (err) {
-      res.status(500).json({ message: "Falha ao criar o card" });
-    }
-  };
+  // static createNewCard = async (req: Request, res: Response) => {
+  //   try {
+  //     const newCarrousselCard = await cardProjeto.create(req.body);
+  //     res.status(200).json(newCarrousselCard);
+  //   } catch (err) {
+  //     res.status(500).json({ message: "Falha ao criar o card" });
+  //   }
+  // };
 
-  static deleteCardById = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    try {
-      await cardProjeto.findByIdAndDelete(id);
-      res.status(200).json({ message: "Card deletado com sucesso" });
-    } catch (err) {
-      res.status(500).json({ message: "Falha ao deletar Card" });
-    }
-  };
+  // static deleteCardById = async (req: Request, res: Response) => {
+  //   const id = req.params.id;
+  //   try {
+  //     await cardProjeto.findByIdAndDelete(id);
+  //     res.status(200).json({ message: "Card deletado com sucesso" });
+  //   } catch (err) {
+  //     res.status(500).json({ message: "Falha ao deletar Card" });
+  //   }
+  // };
 }
--- app/src/routes/CardProjetoRoutes.ts
+++ app/src/routes/CardProjetoRoutes.ts
@@ -4,6 +4,6 @@ import { CardProjetoController } from "../controllers/cardProjetoController";
 export const router = express.Router();
 
   router.get("/cardProjetos", CardProjetoController.listAllCards);
-  router.get("/favoriteCards", CardProjetoController.listFavoriteCards);
-  router.post("/createNewCard", CardProjetoController.createNewCard);
-  router.delete("/deleteCard/:id", CardProjetoController.deleteCardById);
+  // router.get("/favoriteCards", CardProjetoController.listFavoriteCards);
+  // router.post("/createNewCard", CardProjetoController.createNewCard);
+  // router.delete("/deleteCard/:id", CardProjetoController.deleteCardById);
